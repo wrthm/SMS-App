faf92054ac838c732633e170af19d39f505c8c96
Gerald <geraldmir4@gmail.com>
2020-10-11 21:03:40 +0800

    Use TypeScript instead of JS

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,16 +1,18 @@
+import { Client } from "pg"
+
 const { Pool } = require ('pg')
 
 const pool = Pool
 
 module.exports = {
-    async query(text, params) {
+    async query(text: string, params: any) {
       const start = Date.now()
       const res = await pool.query(text, params)
       const duration = Date.now() - start
       console.log('executed query', { text, duration, rows: res.rowCount })
       return res
     },
-    async getClient(callback) {
+    async getClient(callback: any) {
       const client = await pool.connect()
       return client
     }
