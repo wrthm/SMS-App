b030ee9b8b6d9e73ed47294bf2f9590e3d4d1a8b
Gerald <geraldmir4@gmail.com>
2021-03-20 00:00:27 +0800

    Hide topbar

--- src/hostAPIDocs.js
+++ src/hostAPIDocs.js
@@ -10,9 +10,11 @@ const swaggerDocument = yaml.load('./docs/SMS-docs.yml')
 
 const port = 8082
 
+var options = {
+    customCss: '.swagger-ui .topbar { display: none }'
+}
 
-
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options))
 app.listen(port, () => {
     console.log(`API Documentation Server now listening at http://${ip.address()}:${port}`)
 })
