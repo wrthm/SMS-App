66e7523d6d2bbbbea5e7e3817b12262b78ca948c
Gerald <geraldmir4@gmail.com>
2021-03-31 15:27:07 +0800

    Create alias for imported model validator

--- src/components/course/route.ts
+++ src/components/course/route.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express'
 import { CourseController as Controller} from './controller'
 import { validate } from 'express-validation'
-import { CourseValidator, CommonValidator } from '../../utils/validator'
+import { CourseValidator as SchemaValidator, CommonValidator } from '../../utils/validator'
 
 const CourseRouter : Router = Router()
 
 CourseRouter.get('/course/:id', validate(CommonValidator.id), Controller.find)
             .get('/search/course/:name', Controller.find)
             .get('/courses', validate(CommonValidator.pagination), Controller.findAll)
-            .post('/course', validate(CourseValidator.postModel), Controller.add)
-            .put('/course', validate(CourseValidator.model), Controller.update)
+            .post('/course', validate(SchemaValidator.postModel), Controller.add)
+            .put('/course', validate(SchemaValidator.model), Controller.update)
             .delete('/course/:id', validate(CommonValidator.id), Controller.delete)
 
 module.exports = CourseRouter
