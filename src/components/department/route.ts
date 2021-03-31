060c12dd56816f7578222948a3cc3182212625d9
Gerald <geraldmir4@gmail.com>
2021-03-31 21:57:47 +0800

    Align some lines

--- src/components/department/route.ts
+++ src/components/department/route.ts
@@ -6,10 +6,10 @@ import { DepartmentController as Controller} from './controller'
 const DepartmentRouter : Router = Router()
 
 DepartmentRouter.get('/department/:id', validate(CommonValidator.id), Controller.find)
-            .get('/search/department/:name', Controller.find)
-            .get('/departments', validate(CommonValidator.pagination), Controller.findAll)
-            .post('/department', validate(SchemaValidator.postModel), Controller.add)
-            .put('/department', validate(SchemaValidator.model), Controller.update)
-            .delete('/department/:id', validate(CommonValidator.id), Controller.delete)
+                .get('/search/department/:name', Controller.find)
+                .get('/departments', validate(CommonValidator.pagination), Controller.findAll)
+                .post('/department', validate(SchemaValidator.postModel), Controller.add)
+                .put('/department', validate(SchemaValidator.model), Controller.update)
+                .delete('/department/:id', validate(CommonValidator.id), Controller.delete)
 
 module.exports = DepartmentRouter
