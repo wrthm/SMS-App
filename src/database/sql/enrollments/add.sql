12b3e4e6fd4fe3460afbe48c0004e064a7f3480a
Gerald <geraldmir4@gmail.com>
2021-04-09 20:44:05 +0800

    Fix insert SQL query on enrollments

--- src/database/sql/enrollments/add.sql
+++ src/database/sql/enrollments/add.sql
@@ -1,4 +1,4 @@
-INSERT INTO enrollments (academic_term_id, student_id, course_id)
-VALUES (${academic_term_id}, ${student_id}, ${course_id})
+INSERT INTO enrollments (academic_term_id, student_id, course_schedule_id)
+VALUES (${academic_term_id}, ${student_id}, ${course_schedule_id})
 RETURNING id
 ;
