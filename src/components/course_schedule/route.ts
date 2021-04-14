999f1ab77b62cf8d9eb24bf2b114d727d11b955c
Gerald <geraldmir4@gmail.com>
2021-04-15 00:43:41 +0800

    Align indentions

--- src/components/course_schedule/route.ts
+++ src/components/course_schedule/route.ts
@@ -6,10 +6,10 @@ import { CourseScheduleController as Controller} from './controller'
 const CourseScheduleRouter : Router = Router()
 
 CourseScheduleRouter.get('/course_schedule/:id', validate(CommonValidator.id), Controller.find)
-                .get('/search/course_schedules', validate(SchemaValidator.searchArgsModel), Controller.search)
-                .get('/course_schedules', validate(CommonValidator.pagination), Controller.findAll)
-                .post('/course_schedule', validate(SchemaValidator.postModel), Controller.add)
-                .put('/course_schedule', validate(SchemaValidator.model), Controller.update)
-                .delete('/course_schedule/:id', validate(CommonValidator.id), Controller.delete)
+                    .get('/search/course_schedules', validate(SchemaValidator.searchArgsModel), Controller.search)
+                    .get('/course_schedules', validate(CommonValidator.pagination), Controller.findAll)
+                    .post('/course_schedule', validate(SchemaValidator.postModel), Controller.add)
+                    .put('/course_schedule', validate(SchemaValidator.model), Controller.update)
+                    .delete('/course_schedule/:id', validate(CommonValidator.id), Controller.delete)
 
 module.exports = CourseScheduleRouter
--- src/components/course_schedule_content/route.ts
+++ src/components/course_schedule_content/route.ts
@@ -6,8 +6,8 @@ import { CourseScheduleContentController as Controller} from './controller'
 const CourseScheduleContentRouter : Router = Router()
 
 CourseScheduleContentRouter.get('/course_schedule_content/:id', validate(CommonValidator.id), Controller.find)
-                .get('/course_schedule_contents/:cs_id', validate(SchemaValidator.cs_idModel), Controller.listByCourseSchedule)
-                .post('/course_schedule_content', validate(SchemaValidator.postModel), Controller.add)
-                .delete('/course_schedule_content/:id', validate(CommonValidator.id), Controller.delete)
+                           .get('/course_schedule_contents/:cs_id', validate(SchemaValidator.cs_idModel), Controller.listByCourseSchedule)
+                           .post('/course_schedule_content', validate(SchemaValidator.postModel), Controller.add)
+                           .delete('/course_schedule_content/:id', validate(CommonValidator.id), Controller.delete)
 
 module.exports = CourseScheduleContentRouter
