5d36770d7d7d791df86797aca08d9e80436d0c54
Gerald <geraldmir4@gmail.com>
2021-04-24 02:01:53 +0800

    Show all subjects in list of grades of a student, even if subject's grade is empty

--- src/database/sql/grades/listByEnrollmentID.sql
+++ src/database/sql/grades/listByEnrollmentID.sql
@@ -1,6 +1,15 @@
-SELECT g.enrollment_id, g.subject_id, s.name as subject_name, g.grade, g.is_hidden, g.updated_at
-FROM grades g
-INNER JOIN subjects s ON g.subject_id = s.id
-WHERE g.enrollment_id = ${enrollment_id}
-ORDER BY g.updated_at DESC
+SELECT DISTINCT ON (subj.id)
+    subj.id as subject_id,
+    subj.name as subject_name,
+    g.grade,
+    g.is_hidden,
+    g.updated_at
+FROM enrollments e
+INNER JOIN course_schedules c_s ON c_s.id = e.course_schedule_id
+INNER JOIN course_schedules_contents c_s_c ON c_s_c.course_schedule_id = c_s.id
+INNER JOIN schedules sched ON sched.id = c_s_c.schedule_id
+INNER JOIN subjects subj ON subj.id = sched.subject_id
+LEFT JOIN grades g ON g.subject_id = subj.id
+WHERE e.id = ${enrollment_id}
+ORDER BY subj.id ASC
 ;
