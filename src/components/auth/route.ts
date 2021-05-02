90e3521c9048604bb7bebb0b0dc8bdaed669b0b5
Gerald <geraldmir4@gmail.com>
2021-05-02 22:06:31 +0800

    Add authenticateComponentOnly middleware in login endpoint

--- src/components/auth/route.ts
+++ src/components/auth/route.ts
@@ -2,10 +2,11 @@ import { Router } from 'express'
 import { validate } from 'express-validation'
 import { AuthLoginValidator as LoginValidator } from '../../utils/validator'
 import { AuthController as Controller } from './controller'
+import { authenticateComponentOnly } from '../../middleware/authenticate'
 
 const AuthRouter : Router = Router()
 
-AuthRouter.post('/auth/login', validate(LoginValidator.loginModel), Controller.loginDispatcher)
+AuthRouter.post('/auth/login', authenticateComponentOnly, validate(LoginValidator.loginModel), Controller.loginDispatcher)
           .get('/auth/logout', Controller.nope)
           .get('/auth/whoami', Controller.nope)
           .put('/auth/update', validate(LoginValidator.putModel), Controller.update_student_cred)
