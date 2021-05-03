3903b9952318657f64a8170acfa9009fb81c7309
Gerald <geraldmir4@gmail.com>
2021-05-03 16:48:05 +0800

    Remove authenticate middleware wrapper

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -6,61 +6,66 @@ import { UnauthorizedException } from '../exceptions';
 import { DateTime } from 'luxon'
 
 const _authenticateComponentOnly = async (req: Request, res: Response, next: NextFunction) => {
-    const componentKey = req.get('X-Component-Key')
-    let component: number
+    try {
+        const componentKey = req.get('X-Component-Key')
+        let component: number
 
-    if (typeof componentKey === 'string') {
-        const result: system_components_clients | null = await AuthService.component_clients.findByNotRevokedKey(componentKey)
-        if (result) {
-            component = result.component
+        if (typeof componentKey === 'string') {
+            const result: system_components_clients | null = await AuthService.component_clients.findByNotRevokedKey(componentKey)
+            if (result) {
+                component = result.component
+            } else {
+                return next(new UnauthorizedException('Invalid component key'))
+            }
         } else {
-            throw new UnauthorizedException('Invalid component key')
+            component = systemComponentBits.StudentCenter
         }
-    } else {
-        component = systemComponentBits.StudentCenter
+        req.component = component
+    } catch (err) {
+        next(err)
     }
-    req.component = component
 }
 
-const _authenticate = async (req: Request, res: Response, next: NextFunction) => {
-    await _authenticateComponentOnly(req, res, next)
-    const sessionToken = req.get('X-Session-Token')
+export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        await _authenticateComponentOnly(req, res, next)
+        const sessionToken = req.get('X-Session-Token')
 
-    if (typeof sessionToken === 'string') {
-        const result: session | null = await AuthService.sessions.get(sessionToken)
-        if (result) {
-            if (DateTime.fromISO(result.expiration_date) < DateTime.now()) {
-                await AuthService.sessions.revoke(result.session_token)
-                throw new UnauthorizedException('Unauthorized: Session token has expired')
-            }
-            
-            let privilege = 0
-            if (result.type === 'faculty') {
-                let facultyPriv = await AuthService.faculties.getPrivilege(result.id)
-                if (facultyPriv) {
-                    privilege = facultyPriv.privilege
-                } else {
-                    throw new UnauthorizedException('Unauthorized: Invalid faculty ID assigned to session token')
+        if (typeof sessionToken === 'string') {
+            const result: session | null = await AuthService.sessions.get(sessionToken)
+            if (result) {
+                if (DateTime.fromISO(result.expiration_date) < DateTime.now()) {
+                    await AuthService.sessions.revoke(result.session_token)
+                    return next(new UnauthorizedException('Unauthorized: Session token has expired'))
                 }
+                
+                let privilege = 0
+                if (result.type === 'faculty') {
+                    let facultyPriv = await AuthService.faculties.getPrivilege(result.id)
+                    if (facultyPriv) {
+                        privilege = facultyPriv.privilege
+                    } else {
+                        return next(new UnauthorizedException('Unauthorized: Invalid faculty ID assigned to session token'))
+                    }
+                }
+                req.sessionData = {
+                    token: result.session_token,
+                    id: result.id,
+                    type: result.type,
+                    privilege: privilege
+                }
+            } else {
+                return next(new UnauthorizedException('Unauthorized: Invalid session token'))
             }
-            req.sessionData = {
-                token: result.session_token,
-                id: result.id,
-                type: result.type,
-                privilege: privilege
-            }
+            next()
         } else {
-            throw new UnauthorizedException('Unauthorized: Invalid session token')
+            return next(new UnauthorizedException('Unauthorized: Session token not present in HTTP header'))
         }
-    } else {
-        throw new UnauthorizedException('Unauthorized: Session token not present in HTTP header')
+    } catch (err) {
+        next(err)
     }
-}
-
-export const authenticate = (req: Request, res: Response, next: NextFunction) => {
-    _authenticate(req, res, next).then(() => next()).catch((err) => {next(err)});
 }
 
 export const authenticateComponentOnly = (req: Request, res: Response, next: NextFunction) => {
-    _authenticateComponentOnly(req, res, next).then(() => next()).catch((err) => {next(err)});
+    _authenticateComponentOnly(req, res, next).then(() => next()).catch((err) => {next(err)})
 }
