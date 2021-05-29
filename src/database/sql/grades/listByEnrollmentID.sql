0a2fb9915e53570227b0a25e205f0d19b12d2a80
Gerald <geraldmir4@gmail.com>
2021-05-29 14:30:50 +0800

    Fix list of student's grades

--- src/database/sql/grades/listByEnrollmentID.sql
+++ src/database/sql/grades/listByEnrollmentID.sql
@@ -11,7 +11,7 @@ INNER JOIN course_schedules c_s ON c_s.id = e.course_schedule_id
 INNER JOIN course_schedules_contents c_s_c ON c_s_c.course_schedule_id = c_s.id
 INNER JOIN schedules sched ON sched.id = c_s_c.schedule_id
 INNER JOIN subjects subj ON subj.id = sched.subject_id
-LEFT JOIN grades g ON g.subject_id = subj.id
+LEFT JOIN grades g ON g.subject_id = subj.id AND e.id = g.enrollment_id
 WHERE e.id = ${enrollment_id}
 ORDER BY subj.id ASC
 ;
